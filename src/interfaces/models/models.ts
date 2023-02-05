495be2769b8e60750bbd3575f18fbdce7acc4a87
Venya Brodetskiy <benjamincheg@gmail.com>
2023-02-06 00:27:52 +0200

    ex1 added

---
+++ src/interfaces/models/models.ts
@@ -0,0 +1,11 @@
+export interface ICard {
+    imageUrl: string;
+    title: string;
+    description: string;
+}
+
+export interface IHeader {
+    imageUrl: string;
+    title: string;
+    description: string;
+}
